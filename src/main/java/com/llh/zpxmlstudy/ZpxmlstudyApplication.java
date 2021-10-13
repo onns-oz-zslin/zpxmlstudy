d85a2f72739223eeb7bcfb456276d069b62a4954
zpxml <1905282364@qq.com>
2021-10-13 17:39:06 +0800

    '第一次提交'

---
+++ src/main/java/com/llh/zpxmlstudy/ZpxmlstudyApplication.java
@@ -0,0 +1,13 @@
+package com.llh.zpxmlstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpxmlstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZpxmlstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llh/zpxmlstudy/ZpxmlstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llh.zpxmlstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpxmlstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
